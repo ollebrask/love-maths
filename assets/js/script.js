6ae965eb44b05f2191bdf647ee79c9e465f3b349
ollebrask <ollstah@gmail.com>
2024-03-12 13:52:34 +0100

    Add division to game

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,6 +46,8 @@ function runGame(gameType) {
         displayMultiplyQuestion(num1, num2);
     } else if (gameType === "subtract") {
         displaySubtractQuestion(num1, num2);
+    } else if (gameType === "division") {
+        displayDivisionQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -88,6 +90,8 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"];
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
+    } else if (operator === "/") {
+        return [operand1 / operand2, "division"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting`;
@@ -118,7 +122,7 @@ function displayAdditionQuestion(operand1, operand2) {
 
 function displaySubtractQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
-    document.getElementById('operand1').textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
     document.getElementById('operator').textContent = "-";
 }
 
@@ -127,4 +131,23 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "x";
+}
+
+/**
+ * Generates a random number between 1-25
+ */
+function generateRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function displayDivisionQuestion() {
+    let operand2 = generateRandomNumber(1, 25);
+    // Ensure the multiplier combined with operand2 doesn't exceed 25 for operand1
+    let maxMultiplier = Math.floor(300 / operand2);
+    let multiplier = generateRandomNumber(1, maxMultiplier);
+    let operand1 = operand2 * multiplier;
+
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "/";
 }
